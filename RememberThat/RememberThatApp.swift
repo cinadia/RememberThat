a4a37647df825ca1c90a736868262d9be7ab9264
cinadia <misscindycui@gmail.com>
2023-05-24 12:19:51 -0700

    Initial Commit

---
+++ RememberThat/RememberThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberThatApp.swift
+//  RememberThat
+//
+//  Created by Cindy Cui on 2023-05-24.
+//
+
+import SwiftUI
+
+@main
+struct RememberThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
